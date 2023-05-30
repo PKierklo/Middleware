957bf6e6f29be472a0c2273cc7716da254edb5fc
Amonkai <110460985+Amonkai@users.noreply.github.com>
2023-05-30 18:27:44 +0200

    Dodaj pliki projektów.

---
+++ WebApplication3/Middleware/BrowserRedirectMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+namespace WebApplication3.Middleware
+{
+    public class BrowserRedirectMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public BrowserRedirectMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string userAgent = context.Request.Headers["User-Agent"].ToString();
+
+            if (userAgent.Contains("Edge") || userAgent.Contains("Edg") || userAgent.Contains("IE") || userAgent.Contains("Opera GX"))
+            {
+                context.Response.Redirect("https://www.mozilla.org/pl/firefox/new/");
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+    }
+
+    public static class BrowserRedirectMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseBrowserRedirect(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<BrowserRedirectMiddleware>();
+        }
+    }
+}
+
+
